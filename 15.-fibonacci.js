c7b837f16e542604819aca23772fd9326f2851a9
johnston2 <51388064+johnston2@users.noreply.github.com>
2019-09-13 12:32:17 -0500

    Update 15.-fibonacci.js

--- 15.-fibonacci.js
+++ 15.-fibonacci.js
@@ -1,17 +1,18 @@
-let n =peticion(`por favor, introduce un numero`)
+let n =prompt(`por favor, introduce un numero`)
+let xN = parseInt(n,10)
 
-let x = 1
-let y = 1
-var z 
-console.log(x)
-console.log(y)
-z   = y + x;
-console.log(z)
+let a = 1
+let b = 1
+var c 
+console.log(a)
+console.log(b)
+c   = b + a;
+console.log(c)
 for(var i = 3; i <= n-1;   i++)
  {
- x = y;
- y = z;
- z = y + x;
- console.log(z)
+ a = b;
+ b = c;
+ c = b + a;
+ console.log(c)
 
-} 
+}
