57af39115323fb1ffb90d44696dd753c0d06f7ec
johnston2 <51388064+johnston2@users.noreply.github.com>
2019-09-13 12:07:45 -0500

    Add files via upload

---
+++ 15.-fibonacci.js
@@ -0,0 +1,17 @@
+let n =peticion(`por favor, introduce un numero`)
+
+let x = 1
+let y = 1
+var z 
+console.log(x)
+console.log(y)
+z   = y + x;
+console.log(z)
+for(var i = 3; i <= n-1;   i++)
+ {
+ x = y;
+ y = z;
+ z = y + x;
+ console.log(z)
+
+} 
